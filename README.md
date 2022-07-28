2483a330bbb2b69c3d24a5fc7a40eabd7a1249ad
Tristan Shawn den Ouden <trisdo687@gmail.com>
2022-07-28 21:02:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# enoto markdown
+ Web-based markdown editor with an easy-to-use theme editor.
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./templates/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
